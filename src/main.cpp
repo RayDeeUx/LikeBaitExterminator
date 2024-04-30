d1e1949558ba3639e7261edde4d49b62be7d6691
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2024-04-30 13:37:23 -0400

    v1.2.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <Geode/Geode.hpp>
 #include <Geode/modify/CommentCell.hpp>
 #include <Geode/modify/DailyLevelNode.hpp>
 #include <Geode/modify/InfoLayer.hpp>
@@ -9,16 +8,17 @@ using namespace geode::prelude;
 int dailyLevelID = -1;
 int currentLevelID = -1;
 
-const std::regex likebaitComment("(?:(?:[\\S ]+)?(?:here.before|i.?.(?:[\\S ]+)?hack(?:ed)?.(?:this.?comment|my)|(?:(?:\\d?.?)?li?(?:ek|ke)s?\\W?\\W?(?:\\d|if|si|,|=|d|button|this.(?:comment|if)|for|to))|(?:can.i|if.this).?(?:gets?.)?(?:\\d|top)|claim.?your(?:[\\S ]+)?ticket|this.comment.is(?:.not)?.hack(?:ed)?|(?:enough|every.\\d+(?:[\\S ]+)?).li?(?:ek|ke)s?|(?:pl[sz]|get).(?:[\\S ]+)?\\d+.li?(?:ek|ke)s?|this.comment.+li?(?:ek|ke)s?|li?(?:ek|ke)s?(?:[\\S]+)?.(?:or|and).i?|for.(?:[\\S ]+).li?(?:ek|ke)s?|pl[sz].li?(?:ek|ke)s?|i.li?(?:ek|ke)s?.li?(?:ek|ke)s?.?s|if.u.li?(?:ek|ke)s?)(?:[\\S ]+)?|^li?(?:ek|ke)s?.(?:this.)(?:[\\S ]+)?|^(?=if.(?:yo)?u.li?(?:ek|ke)s?.this.then.you(?:.?re?|(?:[\\S ]+)?(?:money|dollars|get|\\$))|i.want.(?:yo)?u(?:[\\S ]+)?li?(?:ek|ke)s?.this)|(?:this.is(?:.an?)?)?.(?:un)?hacked.comment|(?:pl(?:s|z|ease?).)?li?(?:ek|ke)s?(?:.this)?(?:.comment)?.so|gi(?:v|m+i?)e.m(?:ai|y).li?(?:ek|ke)s?|li?(?:ek|ke).my.comment|pass:?.\\d+.li?(?:ek|ke)s?.?(?:pl(?:s|z|ease?)?|so)|(?:[\\S ]+)?comentario(?:[\\S ]+)?hackeado(?:[\\S ]+)?li?(?:ek|ke)s?(?:.infini?tos)?|if.this.gets.li?(?:ek|ke)s?|(?:\\d+).li?(?:ek|ke)s?|unhacked.com+ent|pass.is.\\d+.(?:li?(?:ek|ke)s?|(?:pl(?:s|z|ease?)?|so)))(?:[\\S ]+)?(?:(?!level|and|\\&).)*$", std::regex_constants::icase);
+static const std::regex likebaitCommentRegex("(?:(?:[\\S ]+)?(?:here.before|i.?.(?:[\\S ]+)?hack(?:ed)?.(?:this.?comment|my)|(?:(?:\\d?.?)?li?(?:ek|ke)(?:s|d)?\\W?\\W?(?:\\d|if|si|,|=|button|this.(?:comment|if)|for|to))|(?:can.i|if.this).?(?:[gh]ets?.)?(?:\\d|top)|claim.?your(?:[\\S ]+)?ticket|this.comment.is(?:.not)?.hack(?:ed)?|(?:enough|every.\\d+(?:[\\S ]+)?).li?(?:ek|ke)s?|(?:pl[sz]|get).(?:[\\S ]+)?\\d+.li?(?:ek|ke)s?|this.comment.+li?(?:ek|ke)s?|li?(?:ek|ke)s?(?:[\\S]+)?.(?:or|and).i?|for.(?:[\\S ]+).li?(?:ek|ke)s?|pl[sz].li?(?:ek|ke)s?|i.li?(?:ek|ke)s?.li?(?:ek|ke)s?.?s|if.u.li?(?:ek|ke)s?)(?:[\\S ]+)?|^li?(?:ek|ke)s?.(?:this.)(?:[\\S ]+)?|^(?=if.(?:yo)?u.li?(?:ek|ke)s?.this.then.you(?:.?re?|(?:[\\S ]+)?(?:money|dollars|get|\\$))|i.want.(?:yo)?u(?:[\\S ]+)?li?(?:ek|ke)s?.this)|(?:this.is(?:.an?)?)?.(?:un)?hacked.comment|(?:pl(?:s|z|ease?).)?li?(?:ek|ke)s?(?:.this)?(?:.comment)?.so|gi(?:v|m+i?)e.m(?:ai|y).li?(?:ek|ke)s?|li?(?:ek|ke).my.comment|pass:?.\\d+.li?(?:ek|ke)s?.?(?:pl(?:s|z|ease?)?|so)|(?:[\\S ]+)?comentario(?:[\\S ]+)?hackeado(?:[\\S ]+)?li?(?:ek|ke)s?(?:.infini?tos)?|if.this.gets.li?(?:ek|ke)s?|(?:\\d+).li?(?:ek|ke)s?|unhacked.com+ent|pass.is.\\d+.(?:li?(?:ek|ke)s?|(?:pl(?:s|z|ease?)?|so))|(?:[\\S ]*)?(?:top[\\S ]*comment|before.?rate(?:.ticket)?))(?:[\\S ]+)?(?:(?!level|and|\\&).|)*$", std::regex::optimize | std::regex::icase);
 
 class $modify(CommentCell) {
 	void loadFromComment(GJComment* p0) {
-		auto comment = p0->m_commentString;
 		if ((dailyLevelID != currentLevelID) && Mod::get()->getSettingValue<bool>("onlyOnDaily") && Mod::get()->getSettingValue<bool>("enabled")) {
 			CommentCell::loadFromComment(p0);
 			return;
 		}
-		if (Mod::get()->getSettingValue<bool>("enabled") && (std::regex_match(std::string(comment), likebaitComment)) && p0->m_userID != 16) {
+		std::smatch match;
+		std::string commentString = p0->m_commentString;
+		if (Mod::get()->getSettingValue<bool>("enabled") && (std::regex_match(commentString, match, likebaitCommentRegex)) && p0->m_userID != 16) {
 			if (Mod::get()->getSettingValue<bool>("markAsSpam")) p0->m_isSpam = true;
 			p0->m_likeCount = (p0->m_likeCount) * -10;
 			p0->m_commentString = Mod::get()->getSettingValue<std::string>("replacementText");
